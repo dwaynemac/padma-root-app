c9e9b6ab82e2594bde94ec42266102f50266b288
Dwayne Macgowan <dwaynemac@gmail.com>
2013-08-20 16:39:50 -0300

    preset spec_helper with common configurations
    - include FactoryGirl::Syntax::Methods
    - use :focus symbol to filter specs
    - mock with rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
   #
   # config.mock_with :mocha
   # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -29,10 +29,20 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:each) do
+    Rails.cache.clear
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
